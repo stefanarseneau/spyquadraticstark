fc5b760669853947e174c3b344c8af5635dd41c0
Stefan Arseneau <arseneausm@gmail.com>
2025-01-08 18:51:43 -0500

    regenerating lte

--- stark/measurestark.py
+++ stark/measurestark.py
@@ -32,21 +32,22 @@ def measure_spectrum(wavl, flux, ivar, i, lines = ['a','b','g','d'], lte_mask_si
     nltewindow = {'a' : nlte_core_size, 'b' : nlte_core_size, 'g' : nlte_core_size, 'd' : nlte_core_size}
 
     if modeltype == '1d_da_nlte':
-        ltemodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names = lines, resolution = 0.0637, windows=ltewindow, edges=edges).model
-        nltemodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names = lines, resolution = 0.0637, windows=nltewindow, edges=edges).model
+        ltemodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names=lines, resolution=0.0637, windows=ltewindow, edges=edges).model
+        nltemodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names=lines, resolution=0.0637, windows=nltewindow, edges=edges).model
     elif modeltype == 'voigt':
-        ltemodel = corv.models.make_balmer_model(nvoigt=1, names = lines, windows=ltewindow, edges=edges)
-        nltemodel = corv.models.make_balmer_model(nvoigt=1, names = lines, windows=nltewindow, edges=edges)
+        ltemodel = corv.models.make_balmer_model(nvoigt=1, names=lines, windows=ltewindow, edges=edges)
+        nltemodel = corv.models.make_balmer_model(nvoigt=1, names=lines, windows=nltewindow, edges=edges)
 
     lte_rv, lte_e_rv, lte_redchi, lte_param_res = corv.fit.fit_corv(wavl[ltemask], flux[ltemask], ivar[ltemask], ltemodel)
-    nlte_rv, nlte_e_rv, nlte_redchi, nlte_param_res = corv.fit.fit_corv(wavl, flux, ivar, nltemodel)
+    #nlte_rv, nlte_e_rv, nlte_redchi, nlte_param_res = corv.fit.fit_corv(wavl, flux, ivar, nltemodel)
 
-    #lte_fig = corv.utils.lineplot(wavl[ltemask], flux[ltemask], ivar[ltemask], ltemodel, lte_param_res.params)
-    #nlte_fig = corv.utils.lineplot(wavl, flux, ivar, nltemodel, nlte_param_res.params)
+    lte_figure = corv.utils.lineplot(wavl[ltemask], flux[ltemask], ivar[ltemask], ltemodel, lte_param_res.params)
+    #nlte_figure = corv.utils.lineplot(wavl, flux, ivar, nltemodel, nlte_param_res.params)
+    matplotlib.pyplot.close()
 
     results = {'lte_rv' : [lte_rv], 'lte_e_rv' : [lte_e_rv], 
-               'lte_teff' : [lte_param_res.params['teff'].value], 'lte_logg' : [lte_param_res.params['logg'].value], 'lte_redchi' : [lte_param_res.redchi],
-               'nlte_rv' : [nlte_rv], 'nlte_e_rv' : [nlte_e_rv], 
-               'nlte_teff' : [nlte_param_res.params['teff'].value], 'nlte_logg' : [nlte_param_res.params['logg'].value], 'nlte_redchi' : [nlte_param_res.redchi],
-               'vstark' : [lte_rv - nlte_rv], 'e_vstark' : [np.sqrt(lte_e_rv**2 + nlte_e_rv**2)]}
-    return results
+               'lte_teff' : [lte_param_res.params['teff'].value], 'lte_logg' : [lte_param_res.params['logg'].value], 'lte_redchi' : [lte_param_res.redchi],}
+               #'nlte_rv' : [nlte_rv], 'nlte_e_rv' : [nlte_e_rv], 
+               #'nlte_teff' : [nlte_param_res.params['teff'].value], 'nlte_logg' : [nlte_param_res.params['logg'].value], 'nlte_redchi' : [nlte_param_res.redchi],
+               #'vstark' : [lte_rv - nlte_rv], 'e_vstark' : [np.sqrt(lte_e_rv**2 + nlte_e_rv**2)]}
+    return results, lte_figure#, nlte_figure
--- stark/utils.py
+++ stark/utils.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
 
+def air2vac(wv):
+    _tl=1.e4/np.array(wv)
+    return (np.array(wv)*(1.+6.4328e-5+2.94981e-2/\
+                          (146.-_tl**2)+2.5540e-4/(41.-_tl**2)))
+
 def read_clean_nlte(lines, size):
     goodsystems = pd.read_csv('../data/processed/good_spy.csv').FileName.values
-    nlte = pd.read_csv(f'../data/processed/nlte/{size}angstrom/{lines}.csv')
+    nlte = pd.read_csv(f'../data/processed/nlte/{str(size)}angstrom/{lines}.csv')
     nlte = nlte.query("nlte_logg > 7.1 and nlte_logg < 8.9 and nlte_e_rv < 10 and nlte_redchi < 10")
     return  nlte.loc[nlte['filename'].isin(goodsystems)]
 
 def read_clean_lte(lines, size, window):
     goodsystems = pd.read_csv('../data/processed/good_spy.csv').FileName.values
     lte = pd.read_csv(f'../data/processed/lte/{size}angstrom/{lines}/window_{window}.csv')
-    lte = lte.query("nlte_logg > 7.1 and nlte_logg < 8.9 and nlte_e_rv < 10 and nlte_redchi < 10")
+    lte = lte.query("lte_logg > 7.1 and lte_logg < 8.9 and lte_e_rv < 10 and lte_redchi < 10")
     return  lte.loc[lte['filename'].isin(goodsystems)]
 
 def read_stark_effect(lteargs, nlteargs = None, sigmaclip=True):
@@ -20,7 +25,7 @@ def read_stark_effect(lteargs, nlteargs = None, sigmaclip=True):
     df = pd.merge(nlte, lte, on='filename')
     # compute stark velocities
     df['vstark'] = df['lte_rv'] - df['nlte_rv']
-    df['e_vstark'] = np.sqrt(df['lte_rv']**2 + df['nlte_rv']**2)
+    df['e_vstark'] = np.sqrt(df['lte_e_rv']**2 + df['nlte_e_rv']**2)
     # return
     if sigmaclip:
         sigma_to_clip = 3
